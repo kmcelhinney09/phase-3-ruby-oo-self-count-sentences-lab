4b763726bd24d42b057072173e7769e13b6e69c7
mcelhinneyk <mr.mcelhinney@gmail.com>
2022-01-18 18:35:48 -0800

    commit tree 18389977cd137ed0e6237c244287e7d028e9a511

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/\.|\?|! |" "/)
+    sentences.length
+    # binding.pry
   end
 end
+
+# binding.pry
+# 0
